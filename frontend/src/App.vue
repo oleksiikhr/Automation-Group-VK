a3f8c15b3a45d62006e7b42107ee6b5fb169b848
Alexey Khrusch <alexeykhr@outlook.com>
2018-02-16 18:02:25 +0200

    Replaced frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'App'
 }
 </script>
 
