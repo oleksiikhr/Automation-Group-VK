d937b01336b4d8980275aaba2c5208851b1f7a54
Alexey Khrushch <alexeykhr@outlook.com>
2018-02-27 18:29:19 +0200

    FrontEnd - Basic App.vue template. Other small corrected

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header>
+        
+      </el-header>
+      <el-container>
+        <el-aside width>
+          <el-menu default-active="1" collapse>
+            <el-menu-item index="1">
+              <i class="el-icon-menu"></i>
+              <span slot="title">Меню</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <i class="el-icon-time"></i>
+              <span slot="title">Запуск</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">Navigator Three</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">Navigator Four</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -12,12 +38,18 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+.el-header {
+  padding: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 1000px;
+  margin: 20px auto;
+  border: 1px solid #e7e7e7;
+  box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
 }
 </style>
