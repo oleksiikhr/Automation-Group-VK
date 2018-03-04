db70242d4be5b6033f42ce2089237c8d19ceb19d
Alexey Khrushch <alexeykhr@outlook.com>
2018-03-04 13:15:19 +0200

    Included material icons (link). Completed left-menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,27 +6,17 @@
       </el-header>
       <el-container>
         <el-aside width>
-          <el-menu default-active="1" collapse router>
-            <el-menu-item index="dashboard">
-              <i class="el-icon-menu"></i>
-              <span slot="title">Меню</span>
-            </el-menu-item>
-            <el-menu-item index="2">
-              <i class="el-icon-time"></i>
-              <span slot="title">Статьи</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-              <i class="el-icon-document"></i>
-              <span slot="title">Navigator Three</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <i class="el-icon-setting"></i>
-              <span slot="title">Navigator Four</span>
+          <el-menu default-active="/" collapse router>
+            <el-menu-item v-for="item in menu" :key="item.route" :index="item.route">
+              <i class="material-icons">{{ item.icon }}</i>
+              <span slot="title">{{ item.title }}</span>
             </el-menu-item>
           </el-menu>
         </el-aside>
         <el-main>
-          <router-view />
+          <keep-alive>
+            <router-view />
+          </keep-alive>
         </el-main>
       </el-container>
     </el-container>
@@ -34,12 +24,31 @@
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 
 export default {
-  // mounted () {
-  //   axios.get('groups')
-  // }
+  data () {
+    return {
+      menu: [
+        { route: '/', icon: 'dashboard', title: 'Главная страница' },
+        { route: 'articles', icon: 'description', title: 'Статьи' },
+        { route: 'images', icon: 'photo_library', title: 'Изображения' },
+        { route: 'videos', icon: 'video_library', title: 'Видео' },
+        { route: 'tags', icon: 'label', title: 'Теги' },
+        { route: 'logs', icon: 'history', title: 'Логи' }
+      ]
+    }
+  },
+  mounted () {
+    this.$router.push('/')
+    axios.get('groups')
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err.response.data)
+      })
+  }
 }
 </script>
 
@@ -57,5 +66,13 @@ body {
 
 .el-header {
   padding: 0;
+}
+
+.el-menu-item {
+  text-align: center;
+}
+
+.el-menu-item .material-icons {
+  font-size: 20px;
 }
 </style>
