1d0c8a78eb88376a6a7710add1928d57afa50f3b
Alexey Khrushch <alexeykhr@outlook.com>
2018-03-04 14:03:39 +0200

    Delete collapse in menu. Added header (group), styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <el-container>
-      <el-header>
-
+      <el-header class="blue">
+        <router-link to="groups" class="h-card-group">
+          <!-- TODO Data from vuex -->
+          <img src="https://pp.userapi.com/c637217/v637217600/45610/Zpgr3st-NUw.jpg" alt="__NAME__" title="__NAME__">
+          <div class="h-name">English</div>
+        </router-link>
       </el-header>
       <el-container>
         <el-aside width>
-          <el-menu default-active="/" collapse router>
-            <el-menu-item v-for="item in menu" :key="item.route" :index="item.route">
+          <el-menu default-active="/"  router>
+            <el-menu-item v-for="item in menu" :key="item.route" :index="item.route" :title="item.title">
               <i class="material-icons">{{ item.icon }}</i>
-              <span slot="title">{{ item.title }}</span>
             </el-menu-item>
           </el-menu>
         </el-aside>
@@ -24,7 +27,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
 
 export default {
   data () {
@@ -41,13 +44,13 @@ export default {
   },
   mounted () {
     this.$router.push('/')
-    axios.get('groups')
-      .then(res => {
-        console.log(res.data)
-      })
-      .catch(err => {
-        console.log(err.response.data)
-      })
+    // axios.get('groups')
+    //   .then(res => {
+    //     console.log(res)
+    //   })
+    //   .catch(err => {
+    //     console.log(err)
+    //   })
   }
 }
 </script>
@@ -65,14 +68,46 @@ body {
 }
 
 .el-header {
-  padding: 0;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  background: #545c64;
 }
 
-.el-menu-item {
+.h-card-group {
+  display: flex;
+  flex-direction: row;
+  max-width: 250px;
+  min-width: 150px;
+  background: #fff;
+  height: 35px;
+  align-items: center;
+  cursor: pointer;
+  padding: 0 10px;
+  box-shadow: 0 0 8px 2px #989898;
+  text-decoration: none;
+}
+
+.h-card-group > img {
+  border-radius: 50%;
+  max-height: 27px;
+}
+
+.h-name {
+  align-items: center;
+  text-indent: 5px;
+  color: #525252;
+  font-family: monospace;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+
+.el-aside .el-menu-item {
   text-align: center;
 }
 
-.el-menu-item .material-icons {
+.el-aside .el-menu-item .material-icons {
   font-size: 20px;
 }
 </style>
