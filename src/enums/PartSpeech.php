1dccde20da62d268a81f79d15467fa634e0364da
Alexey Khrusch <alexeykhr@outlook.com>
2018-06-03 17:33:03 +0300

    Added enum - PartSpeech

---
+++ src/enums/PartSpeech.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace src\enums;
+
+class PartSpeech
+{
+    const AUXILIARY_VERB = 'auxiliary verb';
+    const ABBREVIATION = 'abbreviation';
+    const CONJUNCTION = 'conjunction';
+    const PREPOSITION = 'preposition';
+    const ADJECTIVE = 'adjective';
+    const PARTICLE = 'particle';
+    const PRONOUN = 'pronoun';
+    const ADVERB = 'adverb';
+    const NOUN = 'noun';
+    const VERB = 'verb';
+}
