76f63811a8741632a9608a6958e54da55b8f320c
Alexey Khrusch <alexeykhr@outlook.com>
2018-06-05 14:45:14 +0300

    Added Controller.php

---
+++ src/Controller.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace src;
+
+abstract class Controller
+{
+    /**
+     * @var string
+     */
+    protected const HASHTAG = '';
+
+    // todo?
+    protected const SMILE = '';
+
+    /**
+     * Get a hashtag for separating posts and improving search.
+     *
+     * @return string
+     */
+    protected static function getHashtag(): string
+    {
+        return '#' . static::HASHTAG . '@' . G_DOMAIN;
+    }
+
+    /**
+     * todo All attachments (for)*
+     *
+     * @param int $photoId
+     *
+     * @return string
+     */
+    public static function getPhotoAttachment(int $photoId): string
+    {
+        return 'photo-' . G_ID . '_' . $photoId;
+    }
+}
