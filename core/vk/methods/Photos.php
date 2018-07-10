5c67fb833e7c68f82e7e423c501a952d4f312793
Alexey Khrusch <alexeykhr@outlook.com>
2018-07-10 09:06:59 +0300

    Add vk\methods\Photos (4 methods)

---
+++ core/vk/methods/Photos.php
@@ -0,0 +1,91 @@
+<?php declare(strict_types=1);
+
+namespace core\vk\methods;
+
+use core\enums\HttpMethod;
+use core\vk\VK;
+
+class Photos
+{
+    /**
+     * Returns the server address for photo upload onto a user's wall.
+     *
+     * @param  string  $token
+     * @param  int     $groupId
+     * @return mixed
+     * @throws \Exception
+     *
+     * @see https://vk.com/dev/photos.getWallUploadServer
+     */
+    public static function getWallUploadServer(string $token, int $groupId)
+    {
+        return VK::send($token, 'photos.getWallUploadServer', [
+            'group_id' => $groupId,
+        ], HttpMethod::GET);
+    }
+
+    /**
+     * Saves a photo to a user's or community's wall after being uploaded.
+     *
+     * @param  string  $token
+     * @param  string  $photo
+     * @param  string  $server
+     * @param  string  $hash
+     * @return mixed
+     * @throws \Exception
+     *
+     * @see https://vk.com/dev/photos.saveWallPhoto
+     */
+    public static function saveWallPhotoGroup(string $token, string $photo, string $server, string $hash)
+    {
+        return VK::send($token, 'photos.saveWallPhoto', [
+            'group_id' => G_ID,
+            'server'   => $server,
+            'photo'    => $photo,
+            'hash'     => $hash,
+        ], HttpMethod::POST);
+    }
+
+    /**
+     * Receives server address for uploading community cover.
+     *
+     * @param  string  $token
+     * @param  int     $cropX - X coordinate of the left-upper corner.
+     * @param  int     $cropY - Y coordinate of the left-upper corner.
+     * @param  int     $cropX2 - X coordinate of the right-bottom corner.
+     * @param  int     $cropY2 - Y coordinate of the right-bottom corner.
+     * @return mixed
+     * @throws \Exception
+     *
+     * @see https://vk.com/dev/photos.getOwnerCoverPhotoUploadServer
+     */
+    public static function getOwnerCoverPhotoUploadServer(string $token, int $cropX, int $cropY, int $cropX2, int $cropY2)
+    {
+        return VK::send($token, 'photos.getOwnerCoverPhotoUploadServer', [
+            'group_id' => G_ID,
+            'crop_x'   => $cropX,
+            'crop_y'   => $cropY,
+            'crop_x2'  => $cropX2,
+            'crop_y2'  => $cropY2,
+        ], HttpMethod::GET);
+    }
+
+    /**
+     * Saves cover photo after successful uploading.
+     *
+     * @param  string  $token
+     * @param  string  $hash
+     * @param  string  $photo
+     * @return mixed
+     * @throws \Exception
+     *
+     * @see https://vk.com/dev/photos.saveOwnerCoverPhoto
+     */
+    public static function saveOwnerCoverPhoto(string $token, string $hash, string $photo)
+    {
+        return VK::send($token, 'photos.saveOwnerCoverPhoto', [
+            'hash'  => $hash,
+            'photo' => $photo,
+        ], HttpMethod::POST);
+    }
+}
