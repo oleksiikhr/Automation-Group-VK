905ddc8e73750c4bebf472ade583d6bd20b183de
Alexey Khrusch <alexeykhr@outlook.com>
2018-07-22 21:33:31 +0300

    Add PhotosController

---
+++ src/controllers/photos/PhotosController.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace src\controllers\photos;
+
+use src\controllers\FileController;
+use core\vk\enums\Attachment;
+use core\vk\methods\Photos;
+use core\vk\methods\Wall;
+use core\Token;
+use core\Web;
+
+abstract class PhotosController extends FileController implements PhotosInterface
+{
+    /**
+     * @var string
+     */
+    protected $folder;
+
+    public function __construct()
+    {
+        parent::__construct($this->folder);
+    }
+
+    /**
+     * Main method.
+     *
+     * @return void
+     */
+    public function start()
+    {
+        $files = $this->chooseFiles($this->getFilesRecursive($this->pathEntry));
+        $attachments = '';
+
+        if (count($files) < 1) {
+            die('PhotosController - files are empty');
+        }
+
+        foreach ($files as $key => $file) {
+            $uri = realpath($this->pathExit . '/' . $file);
+            try {
+                $server = Photos::getWallUploadServer(Token::getToken());
+                $upload = self::uploadPhoto($server->response->upload_url, $uri);
+                $res = Photos::saveWallPhotoGroup(Token::getToken(), $upload->photo, $upload->server, $upload->hash);
+                $attachments .= Attachment::generate([Attachment::PHOTO => $res->response[0]->id], $res->response[0]->owner_id) . ',';
+            } catch (\Exception $e) {
+                die($e->getMessage());
+            }
+        }
+
+        // TODO Message
+        // TODO Hashtags
+
+        try {
+            Wall::post(Token::getToken(), 'Test', $attachments);
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+    /**
+     * Upload image to server.
+     *
+     * @param  string  $uploadUrl
+     * @param  string  $file - URI
+     * @return object
+     * @throws \Exception
+     */
+    public static function uploadPhoto($uploadUrl, $file): object
+    {
+        $res = Web::uploadFiles($uploadUrl, [
+            'photo' => curl_file_create($file)
+        ]);
+
+        return json_decode($res);
+    }
+}
