186615c33ffdb6be60f306f3f7405aba11978a92
Alexey Khrusch <alexeykhr@outlook.com>
2018-07-28 16:38:19 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,44 +1,70 @@
 <?php declare(strict_types=1);
 
-//use src\controllers\words\types\WordsBadKnowing;
-//use src\controllers\polls\types\PollsTranslate;
-//use src\controllers\words\types\WordsFavorite;
-//use src\controllers\photos\types\PhotosLearn;
-//use src\controllers\polls\types\PollsMissing;
-//use src\controllers\words\types\WordsRepeat;
-//use src\controllers\photos\types\PhotosCard;
-//use src\controllers\photos\types\PhotosJoke;
-//use src\controllers\verbs\VerbsController;
-//use src\controllers\learn\LearnController;
-//use src\controllers\polls\types\PollsFind;
-//use src\controllers\words\types\WordsNew;
+use src\controllers\photos\types as photos;
+use src\controllers\polls\types as polls;
+use src\controllers\words\types as words;
+use src\controllers\verbs;
+use src\controllers\learn;
 
 require_once __DIR__ . '/../server.php';
 
+// .env - APP_SECRET
 if (! \core\Protect::cron()) {
     die;
 }
 
+$h = (int)date('G');
+$m = (int)date('i');
+
+/**
+ * | ------------------------------------------------------------------------
+ * | The file is run using the crown every minute.
+ * | ------------------------------------------------------------------------
+ * |
+ * | @var $h - Hours
+ * | @var $m - Minutes
+ * |
+ */
+
+// TODO Polls - start($photoId)
+// TODO Create table?
+// TODO Change time
+
+if ($m === 0) {
+
+    if     ($h % 3 == 0)
+        (new polls\PollsTranslate)->start();
+
+    elseif ($h % 3 == 1)
+        (new polls\PollsMissing)->start();
+
+    elseif ($h % 3 == 2)
+        (new words\WordsNew(10, 0))->start(456240584);
+
+}
+elseif ($m === 30) {
+
+    if (in_array($h, [12]))
+        (new learn\LearnController)->start(456241870);
+
+    // etc
+
+}
+
 // Polls
-//(new PollsTranslate)->start();
-//(new PollsMissing)->start();
-//(new PollsFind)->start();
+//(new polls\PollsFind)->start();
 
-// Learn
-//(new LearnController)->start(456241870);
-
-// Images
-//(new PhotosCard(mt_rand(1, 10)))->start();
-//(new PhotosJoke(mt_rand(1, 10)))->start();
-//(new PhotosLearn(10))->start();
+// Photos
+//(new photos\PhotosCard(mt_rand(1, 10)))->start();
+//(new photos\PhotosJoke(mt_rand(1, 10)))->start();
+//(new photos\PhotosLearn(10))->start();
 
 // Words
-//(new WordsNew(10, 0))->start(456240584);
-//(new WordsFavorite(10, 0))->start(456240584);
-//(new WordsBadKnowing(10, 0))->start(456240584);
-//(new WordsRepeat(mt_rand(10, 50), mt_rand(0, 20), 10))->start(456240584);
+//(new words\WordsFavorite(10, 0))->start(456240584);
+//(new words\WordsBadKnowing(10, 0))->start(456240584);
+//(new words\WordsRepeat(mt_rand(10, 50), mt_rand(0, 20), 10))->start(456240584);
 
 // Verbs
-//(new VerbsController())->start(456242834);
+//(new verbs\VerbsController())->start(456242834);
 
 echo '<br><br>time: ' . (microtime(true) - APP_START);
